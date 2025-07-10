c84056ecd728475d0f6d34901c743e4a064da9bc
João Victor <victor.jvsb18@gmail.com>
2025-07-10 02:37:20 -0300

    fix: reorder imports and update CORS origin for production

--- src/server.ts
+++ src/server.ts
@@ -7,16 +7,16 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomQuestions } from './http/routes/get-room-questions.ts'
-import { createQuestionRoute } from './http/routes/create-question.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
+  origin: 'https://let-me-ask-front-end.vercel.app/',
 })
 
 app.register(fastifyMultipart)
@@ -34,4 +34,4 @@ app.register(getRoomQuestions)
 app.register(createQuestionRoute)
 app.register(uploadAudioRoute)
 
-app.listen({ port: env.PORT })
+app.listen({ port: env.PORT, host: '0.0.0.0' })
